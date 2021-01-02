2b54e7acc8ff4cdf556e857d67379bf6df737584
sasoRP <pedro.rosas.mcc@gmail.com>
2021-01-02 03:02:44 +0000

    ProCivServer-2: added new fields to the occurrences entity.

--- src/test/java/com/prosas/prociv/server/api/viewModels/OccurrenceTest.java
+++ src/test/java/com/prosas/prociv/server/api/viewModels/OccurrenceTest.java
@@ -5,19 +5,4 @@ import org.junit.jupiter.api.Test;
 
 class OccurrenceTest {
 
-    @Test
-    public void testToString() {
-        // pre-conditions
-        Occurrence occurrence = new Occurrence();
-        occurrence.setExternalId("1");
-        occurrence.setCouncilId(3);
-        occurrence.setDescription("description");
-
-        // action
-        String actual = occurrence.toString();
-
-        // validations
-        String expected = "{ ExternalId: 1, Council: 3, Description: description }\n";
-        assertEquals(expected, actual);
-    }
 }
